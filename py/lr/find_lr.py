bc7f0ad6742e7aeefbf810379b858eadf0720cef
zhujian <505169307@qq.com>
2020-05-02 21:13:18 +0800

    feat(train): 比较不同学习率

--- py/lr/find_lr.py
+++ py/lr/find_lr.py
@@ -7,17 +7,14 @@
 @description: 发现最高学习率
 """
 
-import time
 import math
-import copy
-import os
 import torch
-import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR100
 from torchvision.models import SqueezeNet
+
 from lr import util
 from lr.SmoothLabelCriterion import SmoothLabelCritierion
 
@@ -104,105 +101,6 @@ def find_lr(data_loader, model, criterion, optimizer, device, init_value=1e-8, f
     return log_lrs, losses
 
 
-# def train_model(data_loaders, data_sizes, model_name, model, criterion, optimizer, lr_scheduler,
-#                 num_epochs=25, device=None):
-#     since = time.time()
-#
-#     best_model_weights = copy.deepcopy(model.state_dict())
-#     best_top1_acc = 0.0
-#     best_top5_acc = 0.0
-#
-#     loss_dict = {'train': [], 'test': []}
-#     top1_acc_dict = {'train': [], 'test': []}
-#     top5_acc_dict = {'train': [], 'test': []}
-#     for epoch in range(num_epochs):
-#
-#         print('{} - Epoch {}/{}'.format(model_name, epoch + 1, num_epochs))
-#         print('-' * 10)
-#
-#         # Each epoch has a training and test phase
-#         for phase in ['train', 'test']:
-#             if phase == 'train':
-#                 model.train()  # Set model to training mode
-#             else:
-#                 model.eval()  # Set model to evaluate mode
-#
-#             running_loss = 0.0
-#             # running_corrects = 0
-#             running_top1_acc = 0.0
-#             running_top5_acc = 0.0
-#
-#             # Iterate over data.
-#             for inputs, labels in data_loaders[phase]:
-#                 inputs = inputs.to(device)
-#                 labels = labels.to(device)
-#
-#                 # zero the parameter gradients
-#                 optimizer.zero_grad()
-#
-#                 # forward
-#                 # track history if only in train
-#                 with torch.set_grad_enabled(phase == 'train'):
-#                     if phase == 'test':
-#                         N, N_crops, C, H, W = inputs.size()
-#                         result = model(inputs.view(-1, C, H, W))  # fuse batch size and ncrops
-#                         outputs = result.view(N, N_crops, -1).mean(1)  # avg over crops
-#                     else:
-#                         outputs = model(inputs)
-#                     # print(outputs.shape)
-#                     # _, preds = torch.max(outputs, 1)
-#                     loss = criterion(outputs, labels)
-#
-#                     # compute top-k accuray
-#                     topk_list = metrics.topk_accuracy(outputs, labels, topk=(1, 5))
-#                     running_top1_acc += topk_list[0]
-#                     running_top5_acc += topk_list[1]
-#
-#                     # backward + optimize only if in training phase
-#                     if phase == 'train':
-#                         loss.backward()
-#                         optimizer.step()
-#
-#                 # statistics
-#                 running_loss += loss.item() * inputs.size(0)
-#                 # running_corrects += torch.sum(preds == labels.data)
-#             if phase == 'train':
-#                 lr_scheduler.step(epoch + 1)
-#                 print('lr: {}'.format(lr_scheduler.get_lr()))
-#
-#             epoch_loss = running_loss / data_sizes[phase]
-#             epoch_top1_acc = running_top1_acc / len(data_loaders[phase])
-#             epoch_top5_acc = running_top5_acc / len(data_loaders[phase])
-#
-#             loss_dict[phase].append(epoch_loss)
-#             top1_acc_dict[phase].append(epoch_top1_acc)
-#             top5_acc_dict[phase].append(epoch_top5_acc)
-#
-#             print('{} Loss: {:.4f} Top-1 Acc: {:.4f} Top-5 Acc: {:.4f}'.format(
-#                 phase, epoch_loss, epoch_top1_acc, epoch_top5_acc))
-#
-#             # deep copy the model
-#             if phase == 'test' and epoch_top1_acc > best_top1_acc:
-#                 best_top1_acc = epoch_top1_acc
-#                 best_model_weights = copy.deepcopy(model.state_dict())
-#             if phase == 'test' and epoch_top5_acc > best_top5_acc:
-#                 best_top5_acc = epoch_top5_acc
-#
-#         # 每训练10轮保存一次
-#         if (epoch + 1) % 10 == 0:
-#             util.save_model(model.cpu(), '../data/models/%s_%d.pth' % (model_name, epoch + 1))
-#             model = model.to(device)
-#
-#     time_elapsed = time.time() - since
-#     print('Training {} complete in {:.0f}m {:.0f}s'.format(model_name, time_elapsed // 60, time_elapsed % 60))
-#     print('Best test Top-1 Acc: {:4f}'.format(best_top1_acc))
-#     print('Best test Top-5 Acc: {:4f}'.format(best_top5_acc))
-#
-#     # load best model weights
-#     model.load_state_dict(best_model_weights)
-#     return model, loss_dict, top1_acc_dict, top5_acc_dict
-
-
 if __name__ == '__main__':
     device = util.get_device()
     # device = torch.device('cpu')
@@ -226,24 +124,7 @@ if __name__ == '__main__':
         optimizer = optim.Adam(model.parameters(), lr=1e-8)
 
         print('lr: {}'.format(optimizer.param_groups[0]['lr']))
-        log_lrs, losses = find_lr(data_loaders['train'], model, criterion, optimizer, device)
+        log_lrs, losses = find_lr(data_loaders['train'], model, criterion, optimizer, device,
+                                  init_value=1e-8, final_value=10., beta=0.98)
         print('lr: {}'.format(optimizer.param_groups[0]['lr']))
         util.plot(log_lrs, losses)
-
-        # util.check_dir('../data/models/')
-        # best_model, loss_dict, top1_acc_dict, top5_acc_dict = train_model(
-        #     data_loaders, data_sizes, name, model, criterion, optimizer, lr_scheduler,
-        #     num_epochs=num_epochs, device=device)
-        # 保存最好的模型参数
-        # util.save_model(best_model.cpu(), '../data/models/best_%s.pth' % name)
-
-        # res_loss[name] = loss_dict
-        # res_top1_acc[name] = top1_acc_dict
-        # res_top5_acc[name] = top5_acc_dict
-
-        # print('train %s done' % name)
-        # print()
-
-    # util.save_png('loss', res_loss)
-    # util.save_png('top-1 acc', res_top1_acc)
-    # util.save_png('top-5 acc', res_top5_acc)
--- py/lr/util.py
+++ py/lr/util.py
@@ -8,16 +8,46 @@
 """
 
 import os
-import numpy as np
-import xmltodict
 import torch
-import random
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import MultipleLocator
 
 
 def get_device():
     return torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+def check_dir(data_dir):
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+
+def save_model(model, model_save_path):
+    # 保存最好的模型参数
+    check_dir('./models')
+    torch.save(model.state_dict(), model_save_path)
+
+
+def topk_accuracy(output, target, topk=(1,)):
+    """
+    计算前K个。N表示样本数，C表示类别数
+    :param output: 大小为[N, C]，每行表示该样本计算得到的C个类别概率
+    :param target: 大小为[N]，每行表示指定类别
+    :param topk: tuple，计算前top-k的accuracy
+    :return: list
+    """
+    assert len(output.shape) == 2 and output.shape[0] == target.shape[0]
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, largest=True, sorted=True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 
 def plot(log_lrs, losses):
@@ -33,3 +63,19 @@ def plot(log_lrs, losses):
 
     plt.savefig('./loss-lr.png')
     plt.show()
+
+
+def save_png(title, res_dict):
+    # x_major_locator = MultipleLocator(1)
+    # ax = plt.gca()
+    # ax.xaxis.set_major_locator(x_major_locator)
+    fig = plt.figure()
+
+    plt.title(title)
+    for name, res in res_dict.items():
+        for k, v in res.items():
+            x = list(range(len(v)))
+            plt.plot(v, label='%s-%s' % (name, k))
+
+    plt.legend()
+    plt.savefig('%s.png' % title)
